7c943422547979666f285abd43b81cb909d92c55
genmers <1312625124cxd>
2026-09-18 02:51:27 +0800

    chore: 清理临时诊断探针文件

--- tests/runner/probe2.gd
+++
@@ -1,9 +0,0 @@
-extends SceneTree
-var _body
-func _initialize():
-	_run()
-func _run():
-	await process_frame
-	await process_frame
-	_body = load("res://tests/runner/probe_death_body.gd").new()
-	_body.run(self)
